77eea247e449991cc0839b6977c3b90b752f5f33
Juanesduque1 <juanesduquemorales@gmail.com>
2023-04-04 13:00:34 -0500

    [Fix]: Python route fixed

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
+@app.route("/python/")
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     return 'Python {}'.format(text.replace('_', ' '))
